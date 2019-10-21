6293ea8caf40c4656495e80fbbaacf6e52620000
GrigoroV <grigorov2205valera@gmail.com>
2019-10-22 01:41:56 +0700

    наброски карты

---
+++ Assets/Scripts/ForTests/ButtonStartLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class ButtonStartLevel : MonoBehaviour {
+    [SerializeField] Button _btn       = null;
+    [SerializeField] string _nameScene = "CandyWorld";
+
+    private void Awake() {
+        _btn.onClick.AddListener(() => {
+            SceneManager.LoadScene(_nameScene);
+        });
+    }
+}
---
+++ Assets/Scripts/GameProcess/CameraScroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameProcess {
+    public class CameraScroll : MonoBehaviour {
+        [SerializeField] float _speedScroll = 10f;
+        [SerializeField] float _minY        = 0;
+        [SerializeField] float _maxY        = 0;
+
+        float _startYPosCamera = 0;
+        float _startYPosMouse  = 0;
+
+        void LateUpdate() {
+            if ( Input.GetMouseButtonDown(0) ) {
+                _startYPosCamera = transform.position.y;
+                _startYPosMouse = Input.mousePosition.y;
+            }
+
+            if ( Input.GetMouseButton(0) ) {
+                var curYMouse = Input.mousePosition.y;
+                var yOffset = _startYPosMouse - curYMouse;
+
+                var newPosCamera = transform.position;
+                newPosCamera.y = _startYPosCamera + yOffset * _speedScroll;
+                newPosCamera.y = Mathf.Clamp(newPosCamera.y, _minY, _maxY);
+                transform.position = newPosCamera;
+            }
+        }
+    }
+}

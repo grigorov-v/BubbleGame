48dfcde5064fbdee0cb637cc5a62e7cad15f897f
GrigoroV <grigorov2205valera@gmail.com>
2019-10-27 19:13:32 +0700

    Добавил общий метод для чтения xml

--- Assets/Scripts/Core/XML/LoadHelper.cs
+++ Assets/Scripts/Core/XML/LoadHelper.cs
@@ -6,9 +6,12 @@ namespace Core.XML {
     public class LoadHelper {
         public static void LoadFromResources(IConfig config, string assetPath, string nodeName) {
             var xmlAsset = Resources.Load(assetPath) as TextAsset;
+            LoadConfigFromXmlText(config, xmlAsset.text, nodeName);
+        }
 
+        static void LoadConfigFromXmlText(IConfig config, string xmlText, string nodeName) {
             var xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xmlAsset.text);
+            xmlDoc.LoadXml(xmlText);
 
             var root = xmlDoc.DocumentElement;
             var node = (nodeName == "root") ? (root as XmlNode) : (root[nodeName] as XmlNode);

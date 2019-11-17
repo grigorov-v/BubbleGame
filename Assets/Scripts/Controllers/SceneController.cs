d738e32ea99c2d9912bc0ffae5c1bb647e51002d
GrigoroV <grigorov2205valera@gmail.com>
2019-11-17 20:34:09 +0700

    Переместил скрипты

---
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Core.Controller;
+
+namespace Controllers {
+    public class SceneController : BaseController<SceneController> {
+    }
+}
